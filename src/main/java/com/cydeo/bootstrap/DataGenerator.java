d5f2b20136ff46eb33f34b409e82dfb982a79c9e
Bahadir Turgut <bturgutny@gmail.com>
2023-10-04 06:18:33 -0400

    data generator, dependency injection

---
+++ src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -0,0 +1,42 @@
+package com.cydeo.bootstrap;
+
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.enums.Gender;
+import com.cydeo.service.RoleService;
+import com.cydeo.service.UserService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataGenerator implements CommandLineRunner {
+
+    RoleService roleService;
+    UserService userService;
+
+    public DataGenerator(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        RoleDTO adminRole = new RoleDTO(1L, "Admin");
+        RoleDTO managerRole = new RoleDTO(2L, "Manager");
+        RoleDTO employeeRole = new RoleDTO(3L, "Employee");
+
+        // add data to map -> save()
+
+        roleService.save(adminRole);
+        roleService.save(managerRole);
+        roleService.save(employeeRole);
+
+        UserDTO user1 = new UserDTO("John","Keso","john@cydeo.com","Abc1",true,"745121215",managerRole, Gender.MALE);
+
+
+
+
+    }
+}
