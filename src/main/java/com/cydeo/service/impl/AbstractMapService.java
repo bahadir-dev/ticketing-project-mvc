db16e0c64985c95dd7ee6e8bf9a8007ebd63e762
Bahadir Turgut <bturgutny@gmail.com>
2023-10-07 01:38:04 -0400

    user update

--- src/main/java/com/cydeo/service/impl/AbstractMapService.java
+++ src/main/java/com/cydeo/service/impl/AbstractMapService.java
@@ -22,8 +22,10 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    void deleteById(ID id){
-        map.remove(id);
+    void deleteById(ID id){map.remove(id);}
+
+    void update(ID id, T object){
+        map.put(id,object);
     }
 
 }
